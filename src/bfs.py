8a08ece623fc80a3208bd3eb952dc03dd5fcea6d
Alex <alexandrepinto586@gmail.com>
2024-06-08 17:24:55 -0300

    mudanças do bfs

--- src/bfs.py
+++ src/bfs.py
@@ -1,25 +1,24 @@
 from math import inf
+from src.grafo import Plataforma, Video
+# Algoritmo de busca em profundidade
 
 
-def BFS(n, network, s):
-    '''
-        Algoritmo de busca em profundidade
-    '''
-    color = ["WHITE"]*n
-    d = [inf]*n
-    pi = [-1]*n
+def BFS(plataforma: Plataforma, video_inicio: Video):
+    color = ["WHITE"]*len(plataforma.videos)
+    d = [inf]*len(plataforma.videos)
+    pi = [-1]*len(plataforma.videos)
 
-    color[s] = "GRAY"
-    d[s] = 0
+    color[plataforma.videos.index(video_inicio)] = "GRAY"
+    d[plataforma.videos.index(video_inicio)] = 0
 
-    Q = []
-    Q.append(s)
-    while len(Q) > 0:
-        u = Q.pop(0)
-        for v in range(n):
-            if network[u][v] > 0 and color[v] == "WHITE":
-                color[v] = "GRAY"
-                d[v] = d[u]+1
-                pi[v] = u
-                Q.append(v)
+    # Q = []
+    # Q.append(s)
+    # while len(Q) > 0:
+    #     u = Q.pop(0)
+    #     for v in range(n):
+    #         if plataforma[u][v] > 0 and color[v] == "WHITE":
+    #             color[v] = "GRAY"
+    #             d[v] = d[u]+1
+    #             pi[v] = u
+    #             Q.append(v)
     return d, pi
